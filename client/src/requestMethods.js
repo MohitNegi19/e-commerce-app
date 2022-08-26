f8dd292518bcf37eccce6aecb81e16fe0a60c40e
Mohit Negi <72944509+MohitNegi19@users.noreply.github.com>
2022-08-26 15:19:44 +0530

    h

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN =
+
+const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDY5NGZjMWYwYjEwOGUzYTFmOWY4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTM3NTgyOSwiZXhwIjoxNjYxNjM1MDI5fQ.NFSdwyJIZU7R-xA63n_T_jhfJgYcki2iUQjCKwxr22k"
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
-const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
+//const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+//const currentUser = user && JSON.parse(user).currentUser;
+//const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
